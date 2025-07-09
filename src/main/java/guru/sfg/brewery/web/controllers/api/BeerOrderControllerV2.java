1ee69607a6f213d1075a8e054b949a3de28d0a73
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-07-09 19:27:59 -0300

    user authentication principal on beer order controller v2

---
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderControllerV2.java
@@ -0,0 +1,77 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.security.perms.BeerOrderReadPermissionV2;
+import guru.sfg.brewery.services.BeerOrderService;
+import guru.sfg.brewery.web.model.BeerOrderDto;
+import guru.sfg.brewery.web.model.BeerOrderPagedList;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v2/orders/")
+@RestController
+public class BeerOrderControllerV2 {
+    private static final Integer DEFAULT_PAGE_NUMBER = 0;
+    private static final Integer DEFAULT_PAGE_SIZE = 25;
+
+    private final BeerOrderService beerOrderService;
+
+
+    public BeerOrderControllerV2(BeerOrderService beerOrderService) {
+        this.beerOrderService = beerOrderService;
+    }
+
+    @BeerOrderReadPermissionV2
+    @GetMapping
+    public BeerOrderPagedList listOrders(@AuthenticationPrincipal User user,
+            @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                         @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        if(pageNumber == null || pageNumber < 0) {
+            pageNumber = DEFAULT_PAGE_NUMBER;
+        }
+
+        if(pageSize == null || pageSize < 1) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+        if(user.getCustomer() != null) {
+            return beerOrderService.listOrders(user.getCustomer().getId(), PageRequest.of(pageNumber, pageSize));
+        } else {
+            return beerOrderService.listOrders(PageRequest.of(pageNumber, pageSize));
+        }
+    }
+
+    @BeerOrderCreatePermissionV2
+    @PostMapping("orders")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId, @RequestBody BeerOrderDto beerOrderDto) {
+        return beerOrderService.placeOrder(customerId, beerOrderDto);
+    }
+
+    @BeerOrderReadPermissionV2
+    @GetMapping("orders/{orderId}")
+    public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId) {
+        return beerOrderService.getOrderById(customerId, orderId);
+    }
+
+    @BeerOrderPickupPermissionV2
+    @PutMapping("orders/{orderId}/pickup")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId) {
+        beerOrderService.pickupOrder(customerId, orderId);
+    }
+
+
+}
---
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderPickupPermissionV2.java
@@ -0,0 +1,4 @@
+package guru.sfg.brewery.web.controllers.api;
+
+public @interface BeerOrderPickupPermissionV2 {
+}
