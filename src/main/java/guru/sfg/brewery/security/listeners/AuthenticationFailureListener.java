a7024f66b06066622f008758988a99de32ea193e
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-07-11 21:50:18 -0300

    add authentication failure listener

---
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -0,0 +1,36 @@
+package guru.sfg.brewery.security.listeners;
+
+import guru.sfg.brewery.domain.security.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AuthenticationFailureListener {
+
+    @EventListener
+    public void listen(AuthenticationFailureBadCredentialsEvent event) {
+        log.debug("Login Failure");
+
+        if(event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+
+            if(token.getPrincipal() instanceof String) {
+                log.debug("User name logged in: {}", token.getPrincipal());
+            }
+
+            if(token.getDetails() instanceof WebAuthenticationDetails) {
+                WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+
+                log.debug("Source IP: {}", details.getRemoteAddress());
+            }
+        }
+
+
+    }
+}
